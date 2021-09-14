6de84d2da7e0c39e3b665cc26c48d2042cb7809a
TommyMa99 <ma.jianq@northeasdtern.edu>
2021-09-14 16:09:53 -0700

    adding start of lexxon 1_1

---
+++ leson1_1/app/src/main/java/edu/neu/mad_sea/JianqingMa/lesson1_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.neu.mad_sea.JianqingMa.lesson1_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
